1a3a159ab654cafd3fe7a7fd7df56550f4d890e3
safi <safisafikuls@gmail.com>
2021-06-16 03:10:31 +0600

    update

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
+//login button event handler
 const loginBtn = document.getElementById("login");
 loginBtn.addEventListener("click", function () {
-    console.log("amare click marse.");
-    });
+    const loginArea = document.getElementById("login-area");
+    loginArea.style.display = "none";
+    const transactionArea = document.getElementById("transaction-area");
+    transactionArea.style.display = "block";
+});
+
+//deposit button event handler
+const depositBtn = document.getElementById("addDeposit");
+depositBtn.addEventListener("click", function () {
+    const depositAmount = document.getElementById("depositAmount").value;
+    const depositNumber = parseFloat(depositAmount);
+})
